f71e3c56f9a30ea03143665105504d14b88e1b41
Kyle Truong <ktruong008@gmail.com>
2024-03-25 16:22:23 -0400

    add shadcn-svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,26 @@
+<script lang="ts">
+  import Button from '$lib/components/ui/button/button.svelte';
+  import * as Card from '$lib/components/ui/card';
+
+  let what = 'sdasd';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+
+<Button>Hello</Button>
+
+<Card.Root>
+  <Card.Header>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Description>Card Description</Card.Description>
+  </Card.Header>
+  <Card.Content>
+    <p>Card Content</p>
+  </Card.Content>
+  <Card.Footer>
+    <p>Card Footer</p>
+  </Card.Footer>
+</Card.Root>
