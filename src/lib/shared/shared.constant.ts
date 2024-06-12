32e423747aeb704316d6f3162aeb9cb0f7b84bff
Kyle Truong <ktruong008@gmail.com>
2024-06-12 14:20:08 -0400

    update wallet adapter

--- src/lib/shared/shared.constant.ts
+++ src/lib/shared/shared.constant.ts
@@ -2,7 +2,7 @@ export const what = 'sda';
 
 // Currently deployed on devnet 1.24.0 (May have already been wiped and require redeployment & update depending on date)
 export const MY_FIRST_PACKAGE_ID =
-  '0x83750fbf8aaf6523548ae94f80fa48e5cfaf0fef147fe8c07dc9c7dc79a0f83c';
+  '0x20c97eab2d52d552c43a475652276044aa2be6cc25f5eab98930f477bfa1c72d';
 
 export const SHARED_COUNTER_ID =
-  '0x10ef5f0725f89a4f0f0772aafc1b7a728b26975cac9174b2bf71b6ee4e925572';
+  '0x0058cdde4594f5463ee6228720c5c73d01eb915a44d26136596870644bc3a570';
